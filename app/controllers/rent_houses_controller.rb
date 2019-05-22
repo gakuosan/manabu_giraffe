5b7d8a1174f2f566f29ebd69137997f59bb0ab68
gakuchan <gakuo19831201@gmail.com>
2019-05-22 19:20:14 +0900

    新規登録画面の作成

--- app/controllers/rent_houses_controller.rb
+++ app/controllers/rent_houses_controller.rb
@@ -22,6 +22,7 @@ class RentHousesController < ApplicationController
 
   # GET /rent_houses/1/edit
   def edit
+
   end
 
   # POST /rent_houses
@@ -78,6 +79,6 @@ class RentHousesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def rent_house_params
       params.require(:rent_house).permit(:home_name, :rent, :address, :age, :note, :stations)
-      #stations_attributes:([:home_name,:along_the_line, :station_name, :walking_minutes]))
+      # stations_attributes:([:home_name,:along_the_line, :station_name, :walking_minutes]))
     end
 end
